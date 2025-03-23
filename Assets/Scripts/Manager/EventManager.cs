411877137d49006dbc25b37ce655d5af377e215a
CreatorsDevs <creatorsdevs@gmail.com>
2025-03-24 01:47:12 +0530

    Updated EventManager for handling the base operations via gamemanager and directly connected methods

--- Assets/Scripts/Manager/EventManager.cs
+++ Assets/Scripts/Manager/EventManager.cs
@@ -10,10 +10,43 @@ public class EventManager
     // Camera Actions
     public static Action<int, int> OnOrientationChangeEvent;
 
+    //General Actions
+    public static Action<double> UpdateCurrentBetEvent;
+    public static Action OnSpinClickedEvent;
+    public static Action SpinButtonClickedEvent;
+
+    // Game Economy Actions
+    public static Action<int> UpdateCreditValueIndexEvent;
+    public static Action BalanceAmountDeductionEvent;
+
     #endregion
 
     public static void InvokeOrientationChange(int x, int y)
     {
         OnOrientationChangeEvent(x, y);
+    }
+
+    public static void InvokeCurrentBetUpdateFunctionality(double current_Bet)
+    {
+        UpdateCurrentBetEvent(current_Bet);
+    }
+
+    public static void InvokeUpdateCreditValueIndex(int index)
+    {
+        UpdateCreditValueIndexEvent(index);
+    }
+    public static void InvokeOnSpinClicked()
+    {
+        OnSpinClickedEvent();
+    }
+
+    public static void InvokeSpinButton()
+    {
+        SpinButtonClickedEvent?.Invoke();
+    }
+
+    public static void InvokeUpdateBalanceAmount()
+    {
+        BalanceAmountDeductionEvent();
     }
 }
